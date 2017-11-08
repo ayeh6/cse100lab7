10a4e8410b12f6c360fd4eddc37dde968a9e6b60
Andrew Yeh <ayeh6@ucmerced.edu>
2017-11-08 23:53:44 +0000

    check compare struct

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
-
 using namespace std;
 
 struct Node
 {
 	int id;
 	int freq;
+	int prio;
 	Node *left, *right;
 
-	Node(int id, int freq)
+	Node(int id, int freq, int prio)
 	{
 		left = NULL;
 		right = NULL;
 		this->id = id;
 		this->freq = freq;
+		this->prio = prio;
 	}
 };
 
@@ -27,46 +28,67 @@ struct compare
 	}
 };
 
-void print(struct Node* root, string str)
+void print(struct Node* root, string str, int id, int fq)
 {
-	if(!root)
+	if(root == NULL)
 	{
 		return;
 	}
-	if(root->id != -1)
+	if(root->id == id && root->freq == fq)
 	{
 		cout << str << endl;
 	}
-	
-	print(root->left, str + "0");
-	print(root->right, str + "1");
+
+	print(root->left, str + "0", id, fq);
+	print(root->right, str + "1", id, fq);
 }
 
 void Huffman(int N,int input[],int ID[])
 {
-	struct Node *left, *right, *internal;
+	struct Node *left, *right, *internal, *temp;
 
 	priority_queue<Node*, vector<Node*>, compare> lists;
+
 	for(int i = 0; i < N; i++)
 	{
-		lists.push(new Node(ID[i], input[i]));
+		lists.push(new Node(ID[i], input[i], 0));
 	}
-	
+
 	while(lists.size() != 1)
 	{
 		left = lists.top();
 		lists.pop();
-		
+
 		right = lists.top();
 		lists.pop();
-		
-		internal = new Node(-1, left->freq + right->freq);
-		internal->left = left;
-		internal->right = right;
-		lists.push(internal);
+
+		internal = new Node(-1, left->freq + right->freq, 1);
+		if(left->id == -1 || right->id == -1)
+		{
+			if(left->id == -1)
+			{
+				internal->left = left;
+				internal->right = right;
+				lists.push(internal);
+			}
+			else
+			{
+				internal->right = left;
+				internal->left = right;
+				lists.push(internal);
+			}
+		}
+		else
+		{
+			internal->left = left;
+			internal->right = right;
+			lists.push(internal);
+		}
 	}
-	
-	print(lists.top(), "");
+	for(int i = 0; i < N; i++)
+	{
+		print(lists.top(), "" , ID[i], input[i]);
+	}
 }
 
 
